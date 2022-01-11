737b14d7ed31d99b61552fc5cef4491af787017f
bosoneindustries <bosone.industries@gmail.com>
2022-01-11 21:42:04 +0530

    Login screen made

---
+++ bosone_app/lib/screens/authentication/login.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController _emailField = TextEditingController();
+  TextEditingController _passwordField = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 80),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  'assets/bosone.png',
+                  width: MediaQuery.of(context).size.width / 8,
+                  height: MediaQuery.of(context).size.height / 8,
+                ),
+                Text(
+                  'Bosone.io',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20),
+                ),
+                TextFormField(
+                  controller: _emailField,
+                  decoration: InputDecoration(
+                      hintText: 'example@email.com',
+                      hintStyle: TextStyle(
+                        color: Colors.white,
+                      ),
+                      labelText: 'Email',
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                TextFormField(
+                  controller: _passwordField,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: 'password',
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        color: Colors.black,
+                      )),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.4,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.grey[850],
+                  ),
+                  child: MaterialButton(
+                    onPressed: () async {},
+                    child: Text(
+                      'Register',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.4,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    color: Colors.grey[850],
+                  ),
+                  child: MaterialButton(
+                    onPressed: () async {},
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
